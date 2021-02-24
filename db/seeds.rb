7eb20cf5b60173325cffcbc105625c596022d009
Lauren Miller <lomiller0913@gmail>
2021-02-23 20:04:27 -0500

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Contact.create({first_name: "John", last_name: "Lennon", email: "john@beatles.com", phone_number: "5559876543"})
+
+Contact.create({first_name: "Paul", last_name: "McCartney", email: "paul@beatles.com", phone_number: "5559876544"})
+
+Contact.create({first_name: "Yoko", last_name: "Ono", email: "yoko@beatles.com", phone_number: "5559876444"})
